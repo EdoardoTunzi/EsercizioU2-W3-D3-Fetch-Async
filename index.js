22495b8357375ca5e2ffbb2c52cf3a0aa4e8ddf6
Edoardo <edfl.tunzi@gmail.com>
2024-11-13 15:45:17 +0100

    added fixed height on cards and cards img

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const bookArray = [];
-
 const fetchLibrary = () => {
   fetch("https://striveschool-api.herokuapp.com/books")
     .then((responseObj) => {
@@ -15,17 +13,23 @@ const fetchLibrary = () => {
         const cardDiv = document.createElement("div");
         cardDiv.className = "col";
         cardDiv.innerHTML = `
-            <div class="card">
-             <img src="${book.img}" class="card-img-top" alt="..." />
+            <div class="card h-100" style="min-height: 400px;">
+             <img src="${book.img}" class="card-img-top img-fluid object-fit-scale mt-2" style="height: 400px; alt="Book cover" />
                 <div class="card-body">
-                 <h5 class="card-title">${book.title}</h5>
-                 <p class="card-text">${book.price} €</p>
-                 <a href="#" class="btn btn-primary">Go somewhere</a>
+                 <h5 class="card-title fs-6 text-start">${book.title}</h5>
+                 <p class="card-text fs-3 fw-bolder">${book.price} €</p>
+                 <a href="#" class="btn btn-primary btnDiscard">Discard</a>
                 </div>
           </div>`;
         col.appendChild(cardDiv);
+        const discardBtn = cardDiv.querySelector(".btnDiscard");
+
+        discardBtn.addEventListener("click", () => {
+          cardDiv.remove();
+        });
       });
-    });
+    })
+    .catch((error) => console.log("Errore nel recupero dei dati:", error));
 };
 
 window.onload = () => {
